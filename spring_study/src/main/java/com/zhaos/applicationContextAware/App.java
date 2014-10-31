69e2ad9fd15a9a0f9648fdd5bb6747db27b7685a
zhaoGit <zzbdydfs@126.com>
2014-10-31 23:22:02 +0800

    modify java file

--- spring_study/src/main/java/com/zhaos/applicationContextAware/App.java
+++ spring_study/src/main/java/com/zhaos/applicationContextAware/App.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class App {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args){
 		new ClassPathXmlApplicationContext("com/zhaos/applicationContextAware/applicationContext.xml");
 		String str = "hello spring";
--- spring_study/src/main/java/com/zhaos/applicationContextAware/SpringUtil.java
+++ spring_study/src/main/java/com/zhaos/applicationContextAware/SpringUtil.java
@@ -3,7 +3,7 @@ package com.zhaos.applicationContextAware;
 public class SpringUtil {
 
 	public static int getLength(String str){
-		ToolSpring.getBean("log", MyLog.class).info(str);
+		ToolSpring.getBean("myLog", MyLog.class).info(str);
 		return str.length();
 	}
 }
