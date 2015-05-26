b45d934cd8883029e3bd3d3fe62b1d22993e4a22
zhaoGit <zzbdydfs@126.com>
2015-05-26 15:44:23 +0800

    Add MVC

---
+++ DesignPattern/src/main/java/headfirst/designpatterns/combined/djview/BeatController.java
@@ -0,0 +1,48 @@
+package headfirst.designpatterns.combined.djview;
+  
+public class BeatController implements ControllerInterface {
+	/*
+	 * 持有Model的引用，当Controller解释GUI的动作之后告知Model做相应的动作进行处理
+	 */
+	BeatModelInterface model;
+	/*
+	 * 
+	 */
+	DJView view;
+   
+	public BeatController(BeatModelInterface model) {
+		this.model = model;
+		view = new DJView(this, model);
+        view.createView();
+        view.createControls();
+		view.disableStopMenuItem();
+		view.enableStartMenuItem();
+		model.initialize();
+	}
+  
+	public void start() {
+		model.on();
+		view.disableStartMenuItem();
+		view.enableStopMenuItem();
+	}
+  
+	public void stop() {
+		model.off();
+		view.disableStopMenuItem();
+		view.enableStartMenuItem();
+	}
+    
+	public void increaseBPM() {
+        int bpm = model.getBPM();
+        model.setBPM(bpm + 1);
+	}
+    
+	public void decreaseBPM() {
+        int bpm = model.getBPM();
+        model.setBPM(bpm - 1);
+  	}
+  
+ 	public void setBPM(int bpm) {
+		model.setBPM(bpm);
+	}
+}
