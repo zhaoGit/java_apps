5e18a4c41116d85bc424fbccacfa6b606aeb6822
zhaoGit <zzbdydfs@126.com>
2015-06-02 17:58:57 +0800

    add specification 3

---
+++ DesignPattern/src/main/java/com/zhaos/specification3/UserByAgeThan.java
@@ -0,0 +1,25 @@
+package com.zhaos.specification3;
+
+public class UserByAgeThan extends CompositeSpecification {
+
+	// 基准年龄
+
+	private int age;
+
+	// 构造函数传递基准年龄
+
+	public UserByAgeThan(int _age) {
+
+		this.age = _age;
+
+	}
+
+	// 检验用户是否满足条件
+
+	public boolean isSatisfiedBy(User user) {
+
+		return user.getAge() > age;
+
+	}
+
+}
