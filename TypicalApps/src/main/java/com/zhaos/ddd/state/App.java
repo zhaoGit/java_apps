062b8dfdee249f2f230125894b59881b6bed5c7a
zhaoGit <zzbdydfs@126.com>
2015-06-08 14:58:13 +0800

    Add methods

---
+++ TypicalApps/src/main/java/com/zhaos/ddd/state/App.java
@@ -0,0 +1,9 @@
+package com.zhaos.ddd.state;
+
+public class App {
+	public static void main(String[] args){
+		Reservation reservation = new Reservation();
+//		reservation.accept();
+		reservation.charge();
+	}
+}
--- TypicalApps/src/main/java/com/zhaos/ddd/state/CancelledRso.java
+++ TypicalApps/src/main/java/com/zhaos/ddd/state/CancelledRso.java
@@ -5,19 +5,19 @@ public class CancelledRso implements ReservationStatusOperations {
 	@Override
 	public ReservationStatus accept(Reservation reservation) {
 		// TODO Auto-generated method stub
-		return null;
+		return ReservationStatus.CANCELLED;
 	}
 
 	@Override
 	public ReservationStatus charge(Reservation reservation) {
 		// TODO Auto-generated method stub
-		return null;
+		return ReservationStatus.CANCELLED;
 	}
 
 	@Override
 	public ReservationStatus cancel(Reservation reservation) {
 		// TODO Auto-generated method stub
-		return null;
+		return ReservationStatus.CANCELLED;
 	}
 
 }
