bcb2a7444f8b1dc476ddc5d498fcd0acfa7b117a
zhaoGit <zzbdydfs@126.com>
2015-06-10 10:11:21 +0800

    Add intercepting filter pattern

---
+++ DesignPattern/src/main/java/com/zhaos/intercepting_filter_pattern/App.java
@@ -0,0 +1,13 @@
+package com.zhaos.intercepting_filter_pattern;
+
+public class App {
+	public static void main(String[] args) {
+		FilterManager filterManager = new FilterManager(new Target());
+		filterManager.setFilter(new AuthenticationFilter());
+		filterManager.setFilter(new DebugFilter());
+
+		Client client = new Client();
+		client.setFilterManager(filterManager);
+		client.sendRequest("HOME");
+	}
+}
---
+++ DesignPattern/src/main/java/com/zhaos/intercepting_filter_pattern/Filter.java
@@ -0,0 +1,5 @@
+package com.zhaos.intercepting_filter_pattern;
+
+public interface Filter {
+	public void execute(String request);
+}
