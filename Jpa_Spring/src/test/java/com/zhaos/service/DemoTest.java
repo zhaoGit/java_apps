9ee5185405c341ec85059cf6f0ecf8291d54142c
zhaoGit <zzbdydfs@126.com>
2015-06-15 13:23:54 +0800

    Add unit test

--- Jpa_Spring/src/test/java/com/zhaos/service/DemoTest.java
+++ Jpa_Spring/src/test/java/com/zhaos/service/DemoTest.java
@@ -4,16 +4,35 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.zhaos.domain.ClassEntity;
+import com.zhaos.domain.Person;
+import com.zhaos.utilities.IDGenerator;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:applicationContext.xml"})
 public class DemoTest {
 
+	@Autowired
+	private IPersonService personService;
+
 	@Test
 	public void test() {
-		System.out.println("Not yet implemented");
+		Person person = new Person();
+		ClassEntity classEntity = new ClassEntity();
+
+		person.setId(IDGenerator.getUUID());
+		person.setName("aaa");
+		person.setClassEntity(classEntity);
+		
+		classEntity.setGroupId(IDGenerator.getUUID());
+		classEntity.setName("num 1");
+		classEntity.getPersonList().add(person);
+		
+		personService.saveClassEntity(classEntity);
 	}
 
 }
